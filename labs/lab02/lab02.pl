969bcecae69b5bcd4d4cf4cbb32204159ecfaf0c
abanoub-refaat <abanoubref3at18@gmail.com>
2026-03-01 19:45:11 +0200

    Add lab02

---
+++ labs/lab02/lab02.pl
@@ -0,0 +1,12 @@
+big(bear).              %clause 1
+big(elephant).          %clause 2
+small(cat).             %clause 3
+brown(bear).            %clause 4
+black(cat).             %clause 5
+gray(elephant).         %clause 6
+
+dark(Z) :-              %clause 7
+    black(Z).
+
+dark(Z) :-              %clause 8
+    brown(Z).
